4c7265505edaebd1af5285d4001254d9eb1c3105
Yuuki Kobayashi <y.kobayashi.203@stn.nitech.ac.jp>
2022-07-26 20:46:04 +0900

    modify image url1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from skimage.io import imread
 
 
 # download the image
-img_url = 'https://www.nitech.ac.jp/kikin/'#'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/Mount_Fuji_from_Mount_Aino.jpg/640px-Mount_Fuji_from_Mount_Aino.jpg'
+img_url = 'https://www.nitech.ac.jp/kikin/'
 
 im = imread(img_url)
 
